888972c3d722f0f1d56827a0577de999b12c8dcd
BrandonCHaha <brandon.clark@mscoding.org>
2024-08-02 11:34:23 -0500

    Part 2 of the JavaScript quiz for MCA

---
+++ index.js
@@ -0,0 +1,31 @@
+//let fruits = ["apple", "banana", "cherry"];
+//console.log(fruits[1])
+
+//let numbers = [1, 2, 3, 4, 5];
+//let sum = numbers[0] + numbers[2];
+//console.log(sum)
+
+//let languages = ["JavaScript", "Python", "Java"]
+//languages[1] = "Ruby"
+//console.log(languages)
+
+//Then, use an if-else statement to check if age is greater than or equal to 18, and log "Eligible to vote" if true, and "Not eligible to vote" otherwise.
+//let age = 20;
+//if (age >= 18){
+//    console.log("Eligible to vote");
+//} else {
+//    console.log("Not Elligible to vote");
+//}
+
+//Write a line of code that declares an array called cities with the elements "New York", "Los Angeles", and "Chicago". Then, create a variable lastCity that holds the last element of the array, and log lastCity to the console.
+//let cities = ["New York", "Los Angeles", "Chicago"];
+//let lastCity = cities.pop();
+//console.log(lastCity);
+
+//Write a line of code that declares a variable x and assigns it the value 7. Then, use an if-else statement to check if x is greater than 5 or less than 10, and log "In range" if true, and "Out of range" otherwise.
+let x = 7;
+if (x>5 && x<10){
+    console.log("In range")
+} else {
+    console.log("Out of range")
+}
